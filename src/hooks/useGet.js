4eb4e3c40d22114600a8244ec871facc3f1d256e
Abel <girisha.sangih@gmail.com>
2026-03-28 14:12:29 -0300

    modificaciones de manipulacion de peticiones

--- src/hooks/useGet.js
+++ src/hooks/useGet.js
@@ -5,19 +5,21 @@ import { useEffect, useState } from "react";
 export function useGet(url, initial) {
     const { token } = useAuth();
     const [cargando, setCargando] = useState(true);
-    const [error, setError] = useState(false);
+    const [error, setError] = useState(null);
     const [datos, setDatos] = useState(initial);
 
     useEffect(() => {
         if (url) {
-            get(url, token).then((datos) => {
-                setDatos(datos);
-                setCargando(false);
-                setError(false);
-            }).catch((err) => {
-               setError(err?.message || 'Error al cargar los datos.');
-               setCargando(false);
-           })
+            get(url, token)
+                .then((datos) => {
+                    setDatos(datos);
+                    setCargando(false);
+                    setError(null);
+                })
+                .catch((err) => {
+                    setError(err?.response?.data?.message || err?.message || 'Error al cargar los datos.');
+                    setCargando(false);
+                });
         }
     }, [token, url]);
 
